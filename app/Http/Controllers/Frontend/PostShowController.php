09705a659f5aec7c013770aa2de5b3f0e5464858
aleksandershurdho20 <aleksandershurdho@gmail.com>
2025-04-06 16:47:52 +0200

    feat: added api to view single post

--- app/Http/Controllers/Frontend/PostShowController.php
+++ app/Http/Controllers/Frontend/PostShowController.php
@@ -14,6 +14,19 @@ class PostShowController extends Controller
     public function __invoke(Request $request)
     {
         //
-        return response()->json(Post::with('comments')->get());
+
+        {
+            $query = Post::with('comments');
+                
+            if ($request->has('id')) {
+                $posts = $query->where('id', $request->id)->first();
+                return response()->json($posts);
+            }
+            
+        
+            $posts = $query->get();
+            
+            return response()->json($posts);
+        }
     }
 }
